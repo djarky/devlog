028dc1e4939d48dd57c8feb4d426bd21dbe51b56
djarky <aivr_95@hotmail.com>
2023-09-26 08:20:18 +0000

    Update extracted src.zip

---
+++ src/mygames/bolita/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1580415010,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"images/sprite-sheet2.png",
+		"images/tilemap.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite2-sheet1.png",
+		"images/sprite2-sheet2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
